8109cdeecdc4866a3592ad07b8e3d2faca61de27
Danilo Portillo <38937343+DJPORTILLO30@users.noreply.github.com>
2024-05-12 09:50:54 -0600

    Add exercises from module #3

---
+++ Ejercicios/nameExercise/nameExercise/Program.cs
@@ -0,0 +1,10 @@
+﻿//name
+Console.Write("Escribe tu nombre:");
+var name = Console.ReadLine();
+
+//city
+Console.Write("Escribe tu ciudad:");
+var city = Console.ReadLine();
+
+//final output
+Console.WriteLine("Hola " + name + ", bienvenido a " + city);
