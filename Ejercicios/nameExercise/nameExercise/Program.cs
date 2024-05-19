e846078748a260f4b15175291f8f3b3d22b4b895
Danilo Portillo <dportillo@itland.com.gt>
2024-05-19 12:32:14 -0600

    Code refactor

--- Ejercicios/nameExercise/nameExercise/Program.cs
+++ Ejercicios/nameExercise/nameExercise/Program.cs
@@ -1,10 +1,16 @@
-﻿//name
-Console.Write("Escribe tu nombre:");
-var name = Console.ReadLine();
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //name
+        Console.Write("Escribe tu nombre:");
+        var name = Console.ReadLine();
 
-//city
-Console.Write("Escribe tu ciudad:");
-var city = Console.ReadLine();
+        //city
+        Console.Write("Escribe tu ciudad:");
+        var city = Console.ReadLine();
 
-//final output
-Console.WriteLine("Hola " + name + ", bienvenido a " + city);
+        //final output
+        Console.WriteLine("Hola " + name + ", bienvenido a " + city);
+    }
+}
